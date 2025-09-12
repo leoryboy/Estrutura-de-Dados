319b8a3058412cd9efaaef0032d97def3d56c59c
Leonardo Arruda <la000476@gmail.com>
2025-09-11 22:17:22 -0300

    Aprendendo array

---
+++ array/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    // Declarano e inicializando um array
+    int numero[5] = {10, 20, 30, 40, 50};
+    printf("Elementos do array: %d\n",numero[0]);
+    printf("Elementos do array: %d\n",numero[1]);
+    printf("Elementos do array: %d\n",numero[2]);
+    printf("Elementos do array: %d\n",numero[3]);
+    printf("Elementos do array: %d\n",numero[4]);
+    return 0;
+
+}
